e8f532f36248cdd85fc4d0f2ae49951cedae3022
dusskapark <dusskapark@gmail.com>
2016-07-30 22:31:41 +0900

    express 추가

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
-const hostname = '127.0.0.1';
-const port = 3030;
+const hostname = '221.146.86.186';
+const port = 8080;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
